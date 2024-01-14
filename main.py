861fe95c1a8f080e320a87b2750a4ad7a0ddd115
Dimitrije <jovanovic.dimitrije24@gmail.com>
2024-01-14 16:34:10 +0100

    Random search implemented

--- main.py
+++ main.py
@@ -1,28 +1,88 @@
 import random
+import copy
 from typing import List
+import matplotlib.pyplot as plt
 
-array = [['1','.','.','.'],['.','.','.','.'],['.','.','4','.'],['.','.','.','.']]
+arrayp = [['1','.','.','.'],['.','.','.','.'],['.','.','4','.'],['.','.','.','.']]
 
-def initialize(num_on_board):
-    return [[random.random() < 0.25 for _ in range(int(num_on_board/4))] for _ in range(int(num_on_board/4.0))]
+array = [['1','.','1','.','1','.','2','.','1','1'],
+         ['1','.','.','.','.','.','.','.','.','1'],
+         ['.','.','3','.','.','2','.','.','.','.'],
+         ['.','.','.','.','1','.','.','3','3','.'],
+         ['1','1','1','.','.','.','.','2','.','3'],
+         ['.','.','.','.','0','.','1','.','3','.'],
+         ['.','.','.','1','.','.','2','.','4','.'],
+         ['0','.','.','3','.','.','4','.','.','.'],
+         ['.','3','.','2','.','.','4','.','.','2'],
+         ['.','.','.','.','.','.','.','2','.','0']]
 
-solution = initialize(16)
+def initialize():
+    return [[random.random() < 0.25 for _ in range(int(len(array)))] for _ in range(int(len(array[0])))]
+
+solution = initialize()
 
 def stampaj(niz):
-    for i in range(4):
-        for j in range(4):
-            print(array[i][j],end=' ')
+    n = len(niz)
+    m = len(niz[0])
+    for i in range(n):
+        for j in range(m):
+            print(niz[i][j],end=' ')
         print('\n')
 
 stampaj(array)
 
-def stampaj_modifikacija(niz):
-    for i in range(4):
-        for j in range(4):
-            if array[i][j] == '.' and solution[i][j]:
-                print('X',end=' ')
-            else:
-                print(array[i][j],end=' ')
-        print('\n')
+def change_array(niz,solution):
+    n = len(niz)
+    m = len(niz[0])
+    for i in range(n):
+        for j in range(m):
+            if niz[i][j] == '.' and solution[i][j]:
+                niz[i][j] = 'X'
+    return niz
 
-stampaj_modifikacija(array)
+def count_mines(matrix, i, j):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    count = 0
+
+    for x in range(max(0, i - 1), min(rows, i + 2)):
+        for y in range(max(0, j - 1), min(cols, j + 2)):
+            if (x, y) != (i, j) and matrix[x][y] == 'X':
+                count += 1
+
+    return count
+
+def calc_penalty(matrix):
+    n = len(matrix)
+    m = len(matrix[0])
+    penalty = 0
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] != '.' and matrix[i][j] != 'X':
+                penalty += (int(matrix[i][j]) - count_mines(matrix,i,j))**2
+    return penalty
+
+print(array)
+
+best_one = copy.deepcopy(array)
+best_value = float('inf')
+print('Best value',best_value)
+
+num_iters = 1000000
+values = [None for _ in range(num_iters)]
+
+for i in range(num_iters):
+    solution = initialize()
+    array2 = copy.deepcopy(array)
+    array2 = change_array(array2,solution)
+    penalty = calc_penalty(array2)
+    if penalty < best_value:
+        best_one = copy.deepcopy(array2)
+        best_value = penalty
+    values[i] = best_value
+
+print('Penalty',best_value)
+stampaj(best_one)
+plt.plot(range(num_iters),values)
+plt.show()
