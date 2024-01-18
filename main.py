14a059c873cb9a42568198bf4555eaf284f05756
Dimitrije <jovanovic.dimitrije24@gmail.com>
2024-01-18 13:37:06 +0100

    Changed solution data structure

--- main.py
+++ main.py
@@ -17,72 +17,70 @@ array = [['1','.','1','.','1','.','2','.','1','1'],
          ['.','.','.','.','.','.','.','2','.','0']]
 
 def initialize():
-    return [[random.random() < 0.25 for _ in range(int(len(array)))] for _ in range(int(len(array[0])))]
+    map = {}
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            if array[i][j] == '.':
+                map[(i,j)] = random.random() < 0.25
+    return map
 
-solution = initialize()
-
-def stampaj(niz):
-    n = len(niz)
-    m = len(niz[0])
+def stampaj(solution):
+    n = len(array)
+    m = len(array[0])
     for i in range(n):
         for j in range(m):
-            print(niz[i][j],end=' ')
+            if (i,j) in solution:
+                if solution[(i,j)]:
+                    print('X',end=' ')
+                else:
+                    print('.',end=' ')
+            else:
+                print(array[i][j],end=' ')
         print('\n')
 
-stampaj(array)
-
-def change_array(niz,solution):
-    n = len(niz)
-    m = len(niz[0])
-    for i in range(n):
-        for j in range(m):
-            if niz[i][j] == '.' and solution[i][j]:
-                niz[i][j] = 'X'
-    return niz
-
 def count_mines(matrix, i, j):
-    rows = len(matrix)
-    cols = len(matrix[0])
+    rows = len(array)
+    cols = len(array[0])
     count = 0
 
     for x in range(max(0, i - 1), min(rows, i + 2)):
         for y in range(max(0, j - 1), min(cols, j + 2)):
-            if (x, y) != (i, j) and matrix[x][y] == 'X':
+            if (x, y) != (i, j) and (x,y) in matrix and matrix[(x,y)]:
                 count += 1
 
     return count
 
-def calc_penalty(matrix):
-    n = len(matrix)
-    m = len(matrix[0])
+def calc_penalty(solution):
+    n = len(array)
+    m = len(array[0])
     penalty = 0
 
     for i in range(n):
         for j in range(m):
-            if matrix[i][j] != '.' and matrix[i][j] != 'X':
-                penalty += (int(matrix[i][j]) - count_mines(matrix,i,j))**2
+            if (i,j) not in solution:
+                penalty += (int(array[i][j]) - count_mines(solution,i,j))**2
     return penalty
 
-print(array)
+def random_search():
 
-best_one = copy.deepcopy(array)
-best_value = float('inf')
-print('Best value',best_value)
+    best_one = None
+    best_value = float('inf')
+    #print('Best value',best_value)
 
-num_iters = 1000000
-values = [None for _ in range(num_iters)]
+    num_iters = 1000000
+    values = [None for _ in range(num_iters)]
 
-for i in range(num_iters):
-    solution = initialize()
-    array2 = copy.deepcopy(array)
-    array2 = change_array(array2,solution)
-    penalty = calc_penalty(array2)
-    if penalty < best_value:
-        best_one = copy.deepcopy(array2)
-        best_value = penalty
-    values[i] = best_value
+    for i in range(num_iters):
+        solution = initialize()
+        penalty = calc_penalty(solution)
+        if penalty < best_value:
+            best_one = solution
+            best_value = penalty
+        values[i] = best_value
 
-print('Penalty',best_value)
-stampaj(best_one)
-plt.plot(range(num_iters),values)
-plt.show()
+    stampaj(best_one)
+    print('Penalty',best_value)
+    plt.plot(range(num_iters),values)
+    plt.show()
+
+random_search()
